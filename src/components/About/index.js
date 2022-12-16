d976026e000c881ea2602e8e1d7b80f3d79da2ec
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-12-15 21:20:17 -0500

    error

---
+++ src/components/About/index.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
+
+function About() {
+  const [portfolioPages] = useState([
+    {
+      name: "about Me",
+    },
+  ]);
+
+  const [currentPage] = useState(portfolioPages[0]);
+  return (
+    <div className="masthead text-center text-dark d-flex">
+        <div className="container my-auto">
+            <div className="row">
+                <div className="col-lg-10 mx-auto">
+                    <h1>{capitalizeFirstLetter(currentPage.name)}</h1>
+                    <hr/>
+                    <p>Meeting me is like ordering 10 chickens wings and getting 11. Before start my full stack web journey
+                        I was about in construction building sandhills community college newest addition "Foundation Hall". 
+                        Having the discipline to learn several frontend and backend formats and styles has change the way I think dedicated
+                         </p>
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default About;
+
--- src/components/Pages/index.js
+++ src/components/Pages/index.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import About from '../About';
-import Portfolio from '../Portfolio';
-import Contact from '../Contact';
+// import React from 'react';
+// import About from '../About';
+// import Portfolio from '../Portfolio';
+// import Contact from '../Contact';
 
-function Pages({currentPage}) {
-    const showpage = () => {
-        switch (currentPage.name) {
-            case 'about Me' :
-                return <About Me/>
-                case 'portfolio':
-                    return <Portfolio/>
-                    case 'contact':
-                        return <Contact/>
-                        default:
-                            return <About/> 
-        }
-    };
-    return (
-        showpage()
-    );
-}
+// function Pages({currentPage}) {
+//     const showpage = () => {
+//         switch (currentPage.name) {
+//             case 'about Me' :
+//                 return <About Me/>
+//                 case 'portfolio':
+//                     return <Portfolio/>
+//                     case 'contact':
+//                         return <Contact/>
+//                         default:
+//                             return <About/> 
+//         }
+//     };
+//     return (
+//         showpage()
+//     );
+// }
 
-export default Pages;
+// export default Pages;
