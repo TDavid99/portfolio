40cf6aa0aeac519078882435ac69c9340dbb1d2c
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-12-15 22:25:26 -0500

    working in component footer

--- src/components/Pages/index.js
+++ src/components/Pages/index.js
@@ -1,24 +1,32 @@
-// import React from 'react';
-// import About from '../About';
-// import Portfolio from '../Portfolio';
-// import Contact from '../Contact';
+import React from 'react';
+import About from '../About';
+import Portfolio from '../Portfolio';
+import Contact from '../Contact';
 
-// function Pages({currentPage}) {
-//     const showpage = () => {
-//         switch (currentPage.name) {
-//             case 'about Me' :
-//                 return <About Me/>
-//                 case 'portfolio':
-//                     return <Portfolio/>
-//                     case 'contact':
-//                         return <Contact/>
-//                         default:
-//                             return <About/> 
-//         }
-//     };
-//     return (
-//         showpage()
-//     );
-// }
+function Pages({currentPage}) {
+    const showpage = () => {
+        switch (currentPage.name) {
+            case 'about Me' :
+                return <About Me/>
+                case 'portfolio':
+                    return <Portfolio/>
+                    case 'contact':
+                        return <Contact/>
+                        default:
+                            return <About/> 
+        }
+    };
+    return (
+        showpage()
+    );
+}
 
-// export default Pages;
+
+
+function Pages() {
+    return (
+        <div> This is pages...</div>
+        
+        )
+    }
+    export default Pages;
