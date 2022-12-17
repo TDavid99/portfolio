13a7c5fe544131a5c97c5f437c95316885af5846
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-12-16 19:09:37 -0500

    working on resume index

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -11,7 +11,18 @@ function Resume () {
     const [currentPage] = useState(portfolioPages[0]);
     return (
         <section className="bg-dark">
-            <div className="container"></div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-10 mx-auto text-light mb-4">
+                        <h1 className="text-center mt-5">{capitalizeFirstLetter(currentPage.name)}</h1>
+                        <hr className="light my-4"/>
+                        <p className="text-center">
+                            Upload my <a href={resume} className='text-faded white-link' download>resume</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
         </section>
-    )
+    );
 }
+export default Resume; 
