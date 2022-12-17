ed6c3e13aa55dd8516aedf66cf2351fbb5b8b5f5
townsendda@sandhills.edu <108563194+TDavid99@users.noreply.github.com>
2022-12-17 10:45:01 -0500

    working in project index

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Hi welcome to my portfolio. Lets get to David. There information about my career and ways to contact Tdavid. Please reach out for more details about me. 
 
 # Made with React
+
+
+# Contributors
+Tdavid99
+
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,100 +1,60 @@
-import React from 'react';
-import wishagram from '../../assets/img/apps/Screenshot (429).png';
-import tech from '../../assets/img/apps/Screenshot (428).png'; 
-import password from '../../assets/img/apps/Screenshot (427).png';
-import weather from '../../assets/img/apps/Screenshot (430).png' ;
-import notes from '../../assets/img/apps/notetaker.png';
+import React,{useState} from 'react';
+import { capitalizeFirstLetter } from '../../assets/utils/helpers';
 
-const Projects =() => {
-    const portfolioPages = [
-        {
-            id: 1,
-            source: notes,
-            name: 'Note Taker',
-            repo: 'https://github.com/TDavid99/note-taker',
-            link: ' https://note-takerlol.herokuapp.com/',
-          },
-          {
-            id: 2,
+function Projects ()  {
+    const [portfolioPages ]= useState([
+      {
+        name:'projects'
+      }
+    ]);
+    const [currentPage]= useState(portfolioPages[0]);
+    const [projects] = useState([
+
+      {
+        image: require('../../assets/img/apps/').default,
+        source: notes,
+        name: 'Note Taker',
+        repo: 'https://github.com/TDavid99/note-taker',
+        link: ' https://note-takerlol.herokuapp.com/',
+      },
+      {
+            image: require('../../assets/img/apps/').default,
             source: password,
             name: 'Password Generator',
             repo: 'https://github.com/TDavid99/password-generator',
             link: 'https://github.com/TDavid99/password-generator#httpstdavid99githubiopassword-generator',
           },
           {
-            id: 3,
+            image: require('../../assets/img/apps/').default,
             source: weather,
             name: 'Weather on the go',
             repo: 'https://github.com/CalebFunderburk/On-The-Go',
             link: 'https://calebfunderburk.github.io/On-The-Go/',
           },
           {
-            id: 4,
+            image: require('../../assets/img/apps/').default,
             source: '',
             name: '',
             repo: '',
             link: '',
           },
           {
-            id: 5,
-            src: tech,
+            image: require('../../assets/img/apps/').default,
+            source: tech,
             name: 'Tech Blog',
             repo: 'https://github.com/TDavid99/tech-blog',
             link: 'https://afternoon-falls-50697.herokuapp.com/',
           },
           {
-            id: 6,
+            image: require('../../assets/img/apps/').default,
             source: wishagram,
             name: 'Wishagram',
             repo: 'https://github.com/pseudoDjnn/Wishagram',
             link: 'https://glacial-mesa-81435.herokuapp.com/',
           },
-        ];
-        return (
-            <div
-            name="portfolio"
-            className="bg-gradient-from-neutral-800 via-zinc-700 to-neutral-900 w-full md:h-screen"
-          >
-            <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-              <div className="pb-8">
-                <p className="text-4xl font-bold inline border-b-4">
-                  Portfolio Projects
-                </p>
-                <p className="py-8">
-                 projects for my 2022 portfolio.
-                </p>
-              </div>
+        ]);
       
-              <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-                {portfolioPages.map(({ id, source, name, link, repo }) => (
-                  <div key={id} className="shadow-md shadow-slate-600 rounded-lg">
-                    <img
-                      src={source}
-                      alt="demo"
-                      className="rounded-md duration-700 hover:scale-105"
-                    />
-                    <div className="flex items-center justify-center">
-                      <a
-                        href={repo}
-                        className="w-1/2 px-6 py-2 m-4 duration-300 hover:scale-105"
-                      >
-                        {name}
-                      </a>
-                      <a
-                        href={link}
-                        className="w-1/2 px-6 py-2 m-4 duration-300 hover:scale-105"
-                      >
-                        Deployment
-                      </a>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-        );
-      };
-                
+        
+        
 
-
-export default Projects; 
+export default Projects;
